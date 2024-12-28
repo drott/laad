12d5f0453cb98e7267d58a30b2e09892224625d0
Dominik Röttsches <d@rtsh.es>
2024-12-28 18:30:17 +0200

    Log more

--- src_server/main.rs
+++ src_server/main.rs
@@ -78,7 +78,7 @@ fn make_service() -> Service {
 #[tokio::main]
 async fn main() {
     let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::INFO)
+        .with_max_level(Level::DEBUG)
         .finish();
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
